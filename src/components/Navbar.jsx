aa0c74e7cdbcb88269884453519485bcbdf44e7a
Manpreet <manpreetjaur11406@gmail.com>
2026-08-13 14:13:08 +0530

    Update mobile responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,17 @@ import "./Navbar.css";
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
 
       {/* =========================
           BROWSE CATEGORIES
       ========================= */}
+
       <button
         type="button"
         className="browse-categories"
@@ -32,27 +37,63 @@ function Navbar() {
         />
       </button>
 
+
       {/* =========================
-          MAIN NAVIGATION
+          DESKTOP NAVIGATION
       ========================= */}
+
       <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/contact">Contact Us</Link>
+
+        <Link to="/" onClick={closeMenu}>
+          Home
+        </Link>
+
+        <Link to="/products" onClick={closeMenu}>
+          Products
+        </Link>
+
+        <Link to="/about" onClick={closeMenu}>
+          About Us
+        </Link>
+
+        <Link to="/blog" onClick={closeMenu}>
+          Blog
+        </Link>
+
+        <Link to="/contact" onClick={closeMenu}>
+          Contact Us
+        </Link>
+
       </div>
+
 
       {/* =========================
           CATEGORY DROPDOWN
       ========================= */}
+
       {menuOpen && (
         <div className="category-menu">
-          <Link to="/products">All Products</Link>
-          <Link to="/products">Engine Parts</Link>
-          <Link to="/products">Filters</Link>
-          <Link to="/products">Brake Parts</Link>
-          <Link to="/products">Truck Accessories</Link>
+
+          <Link to="/products" onClick={closeMenu}>
+            All Products
+          </Link>
+
+          <Link to="/products" onClick={closeMenu}>
+            Engine Parts
+          </Link>
+
+          <Link to="/products" onClick={closeMenu}>
+            Filters
+          </Link>
+
+          <Link to="/products" onClick={closeMenu}>
+            Brake Parts
+          </Link>
+
+          <Link to="/products" onClick={closeMenu}>
+            Truck Accessories
+          </Link>
+
         </div>
       )}
 
